cef2f8b86da60365a72c2b75e0da0d0c06e16432
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-21 13:24:38 +0530

    Update run_classifier.py

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -54,16 +54,16 @@ def main():
     parser = argparse.ArgumentParser()
 
     ## Required parameters
-    parser.add_argument("--data_dir",
+    parser.add_argument("",
                         default=None,
                         type=str,
                         required=True,
                         help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
-    parser.add_argument("--bert_model", default=None, type=str, required=True,
+    parser.add_argument("bert-base-uncased", default=None, type=str, required=True,
                         help="Bert pre-trained model selected in the list: bert-base-uncased, "
                         "bert-large-uncased, bert-base-cased, bert-large-cased, bert-base-multilingual-uncased, "
                         "bert-base-multilingual-cased, bert-base-chinese.")
-    parser.add_argument("--task_name",
+    parser.add_argument("task_name",
                         default=None,
                         type=str,
                         required=True,
